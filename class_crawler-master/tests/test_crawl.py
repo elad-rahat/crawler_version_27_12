d45b52f3584f29120f14ecf4dc1fe83984370e30
Yoav Kleinberger <haarcuba@gmail.com>
2023-12-27 17:44:14 +0200

    code review

--- class_crawler-master/tests/test_crawl.py
+++ class_crawler-master/tests/test_crawl.py
@@ -59,7 +59,7 @@ def test_edge_case__depth_3_but_max_depth_is_2():
         s.find_links('link1') >> []
         s.find_links('link2') >> ['link2.1']
 
-        tested = crawler.crawl.Crawl('root', 2, ingore_regex=Fake('ingore_regex'), find_urls=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', max_depth=2, ingore_regex=Fake('ingore_regex'), find_urls=Fake('find_links'))
         assert tested.web_of_links() == [
                 ['root'],
                 ['link1', 'link2'],
@@ -74,7 +74,7 @@ def test_depth_4_link_tree_but_depth_is_3():
         s.find_links('link1.2') >> ['link1.2.1']
         s.find_links('link2.1') >> []
 
-        tested = crawler.crawl.Crawl('root', 3, ingore_regex=Fake('ingore_regex'), find_urls=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', max_depth=3, ingore_regex=Fake('ingore_regex'), find_urls=Fake('find_links'))
         assert tested.web_of_links() == [
                 ['root'],
                 ['link1', 'link2'],
@@ -82,23 +82,23 @@ def test_depth_4_link_tree_but_depth_is_3():
            ]
 
 
-def test_depth_2_link_tree_with_ingore_regex_start_l():
+def test_depth_2_link_tree_with_ingore_regex():
     with Scenario() as s:
         s.find_links('root') >> ['link1', 'link2']
 
 
-        tested = crawler.crawl.Crawl('root', 2, "^l", find_urls=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', max_depth=2, ingore_regex="^l", find_urls=Fake('find_links'))
         assert tested.web_of_links() == [
                 ['root'],
            ]
 
-def test_depth_2_link_tree_with_ingore_regex():
+def test_ignore_regex_never_matches_any_link__all_links_visited():
     with Scenario() as s:
         s.find_links('root') >> ['link1', 'link2']
         s.find_links('link1') >> []
         s.find_links('link2') >> []
 
-        tested = crawler.crawl.Crawl('root', 2, "^n", find_urls=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', max_depth=100, ingore_regex="^n", find_urls=Fake('find_links'))
         assert tested.web_of_links() == [
                 ['root'],
                 ['link1', 'link2'],
@@ -106,7 +106,7 @@ def test_depth_2_link_tree_with_ingore_regex():
 
 
 
-def test_depth_4_link_tree_with_ingore_regex_start_link1():
+def test_depth_4_link_tree_with_ingore_regex():
     with Scenario() as s:
         s.find_links('root') >> ['link1', 'link2']
         s.find_links('link2') >> ['link2.1']
@@ -128,32 +128,13 @@ def test_depth_4_link_tree_with_ingore_regex_start_link11():
         s.find_links('link2.1') >> []
         s.find_links('link1.2.1') >> []
 
-        tested = crawler.crawl.Crawl('root', 4, "^link1.1", find_urls=Fake('find_links'))
+        tested = crawler.crawl.Crawl('root', 4, "^link1\.1", find_urls=Fake('find_links'))
         assert tested.web_of_links() == [
                 ['root'],
                 ['link1', 'link2'],
                 ['link1.2', 'link2.1'],
                 ['link1.2.1',]
            ]
-
-
-def test_depth_4_link_tree_with_ingore_regex_start_link12():
-    with Scenario() as s:
-        s.find_links('root') >> ['link1', 'link2']
-        s.find_links('link1') >> ['link1.1', 'link1.2']
-        s.find_links('link2') >> ['link2.1']
-        s.find_links('link1.1') >> []
-        s.find_links('link1.2') >> ['link1.2.1']
-        s.find_links('link1.2.1') >> []
-
-        tested = crawler.crawl.Crawl('root', 4, "link2.1", find_urls=Fake('find_links'))
-        assert tested.web_of_links() == [
-            ['root'],
-            ['link1', 'link2'],
-            ['link1.1', 'link1.2'],
-            ['link1.2.1', ]
-        ]
-
 
 def test_depth_4_link_tree_with_ingore_regex_mean_nothing():
     with Scenario() as s:
@@ -172,5 +153,3 @@ def test_depth_4_link_tree_with_ingore_regex_mean_nothing():
                 ['link1.1', 'link1.2', 'link2.1'],
                 ['link1.2.1',]
            ]
-
-
